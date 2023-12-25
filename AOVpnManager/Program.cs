25309ce2e4327e4b4bc35ca06437d59f6016a10a
rinrab <tima@chemodax.net>
2023-12-25 18:08:17 +0100

    Add project files.

---
+++ AOVpnManager/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AOVpnManager
+{
+    public class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+
+            using (Process process = Process.Start("mspaint"))
+            {
+                process.WaitForExit();
+            }
+
+            Console.WriteLine("Goodbye, world!");
+
+            return 0;
+        }
+    }
+}
