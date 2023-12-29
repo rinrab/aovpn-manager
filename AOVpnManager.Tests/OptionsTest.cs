2d29db31a009d904360b9f565ea83f60a82a4874
rinrab <tima@chemodax.net>
2023-12-29 17:39:48 +0100

    Add tests.

---
+++ AOVpnManager.Tests/OptionsTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AOVpnManager.Tests
+{
+    [TestClass]
+    public class OptionsTest
+    {
+        [TestMethod]
+        public void ReadFromArgsTest()
+        {
+            Assert.AreEqual(true, Options.ReadFromArgs(new string[] { "/console" }).IsConsole);
+            Assert.AreEqual(false, Options.ReadFromArgs(new string[] { "/test" }).IsConsole);
+            Assert.AreEqual(false, Options.ReadFromArgs(Array.Empty<string>()).IsConsole);
+            // TODO: Assert.AreEqual(true, Options.ReadFromArgs(new string[] { "/test", "/Console" }).IsConsole);
+        }
+    }
+}
