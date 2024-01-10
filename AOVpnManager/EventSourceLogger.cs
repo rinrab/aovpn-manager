20668f4f59c9b7cdc32441434a613dc5d5c86d4f
rinrab <tima@chemodax.net>
2024-01-10 16:32:48 +0100

    Merge Operation chanel to Admin.

--- AOVpnManager/EventSourceLogger.cs
+++ AOVpnManager/EventSourceLogger.cs
@@ -5,13 +5,13 @@ namespace AOVpnManager
     [EventSource(Name = "AOVpnManager", Guid = "73C709D4-5F64-44FD-8402-E4D25FDC90CC")]
     public sealed class EventSourceLogger : EventSource, ILogger
     {
-        [Event(1, Level = EventLevel.Informational, Channel = EventChannel.Operational, Message = "AOVpnManager started")]
+        [Event(1, Level = EventLevel.Informational, Channel = EventChannel.Admin, Message = "AOVpnManager started")]
         public void Started()
         {
             WriteEvent(1);
         }
 
-        [Event(2, Level = EventLevel.Informational, Channel = EventChannel.Operational, Message = "AOVpnManager finished with code {0}")]
+        [Event(2, Level = EventLevel.Informational, Channel = EventChannel.Admin, Message = "AOVpnManager finished with code {0}")]
         public void Finished(int code)
         {
             WriteEvent(2, code);
@@ -23,13 +23,13 @@ namespace AOVpnManager
             WriteEvent(3, message, stackTrace);
         }
 
-        [Event(4, Level = EventLevel.Informational, Channel = EventChannel.Operational, Message = "Vpn connection \"{0}\" created.")]
+        [Event(4, Level = EventLevel.Informational, Channel = EventChannel.Admin, Message = "Vpn connection \"{0}\" created.")]
         public void VpnConnectionCreated(string connectionName)
         {
             WriteEvent(4, connectionName);
         }
 
-        [Event(6, Level = EventLevel.Informational, Channel = EventChannel.Operational, Message = "Vpn connection \"{0}\" deleted.")]
+        [Event(6, Level = EventLevel.Informational, Channel = EventChannel.Admin, Message = "Vpn connection \"{0}\" deleted.")]
         public void VpnConnectionDeleted(string connectionName)
         {
             WriteEvent(6, connectionName);
