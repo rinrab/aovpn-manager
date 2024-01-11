3e82f1183a3f91b891a6e2f7ac24f1e5157112d6
rinrab <tima@chemodax.net>
2024-01-11 15:37:54 +0100

    Remove constructor without connectionName

--- AOVpnManager/VpnConnectionAlreadyExistsException.cs
+++ AOVpnManager/VpnConnectionAlreadyExistsException.cs
@@ -5,10 +5,6 @@ namespace AOVpnManager
     [Serializable]
     internal class VpnConnectionAlreadyExistsException : Exception
     {
-        public VpnConnectionAlreadyExistsException() : base("Vpn Connection already exists.")
-        {
-        }
-
         public VpnConnectionAlreadyExistsException(string connectionName) :
             base(string.Format("Vpn Connection '{0}' already exists.", connectionName))
         {
--- AOVpnManager/VpnConnectionNotFoundException.cs
+++ AOVpnManager/VpnConnectionNotFoundException.cs
@@ -4,10 +4,6 @@ namespace AOVpnManager
 {
     public class VpnConnectionNotFoundException : Exception
     {
-        public VpnConnectionNotFoundException() : base("Vpn Connection not found.")
-        {
-        }
-
         public VpnConnectionNotFoundException(string connectionName) :
             base(string.Format("Vpn Connection '{0}' not found.", connectionName))
         {
