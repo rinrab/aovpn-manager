8f3f203e4d19227ae43247d3b3dac47395a13cce
rinrab <tima@chemodax.net>
2024-01-13 12:40:40 +0100

    Add ListConnectionsPage

---
+++ AdvancedVpnManager/ListConnectionsPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace AdvancedVpnManager
+{
+    public sealed partial class ListConnectionsPage : Page
+    {
+        private readonly ListConnectionsViewModel viewModel;
+
+        public ListConnectionsPage()
+        {
+            InitializeComponent();
+
+            viewModel = new ListConnectionsViewModel();
+            DataContext = viewModel;
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            await viewModel.RefreshAsync();
+        }
+    }
+}
---
+++ AdvancedVpnManager/ListConnectionsViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Windows.Networking.Vpn;
+
+namespace AdvancedVpnManager
+{
+    public class ListConnectionsViewModel : INotifyPropertyChanged
+    {
+        private readonly VpnManagementAgent vpnAgent;
+        private VpnConnection[] vpnConnections;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ListConnectionsViewModel()
+        {
+            vpnAgent = new VpnManagementAgent();
+        }
+
+        public VpnConnection[] VpnConnections
+        {
+            get
+            {
+                return vpnConnections;
+            }
+        }
+
+        public async Task RefreshAsync()
+        {
+            vpnConnections = await GetVpnConnections();
+
+            InvokePropertyChanged(nameof(VpnConnections));
+        }
+
+        private async Task<VpnConnection[]> GetVpnConnections()
+        {
+            var profiles = await vpnAgent.GetProfilesAsync();
+
+            List<VpnConnection> rv = new List<VpnConnection>(profiles.Count);
+
+            foreach (IVpnProfile profile in profiles)
+            {
+                rv.Add(new VpnConnection
+                {
+                    Name = profile.ProfileName
+                });
+            }
+
+            return rv.ToArray();
+        }
+
+        private void InvokePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class VpnConnection
+    {
+        public string Name { get; set; }
+    }
+}
